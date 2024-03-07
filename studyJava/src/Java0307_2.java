47cd3cf081c23860581691ae2bfa037fc5451ffd
SeungHwanYuk <ysh54244902@gmail.com>
2024-03-07 17:19:34 +0900

    0307

---
+++ studyJava/src/Java0307_2.java
@@ -0,0 +1,21 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Java0307_2 {
+    public static void main(String[] args) {
+        Random rd = new Random();
+        int[] lottoList = new int[6];
+        for (int i = 0; i < lottoList.length; i++) {
+            int temp = rd.nextInt(45) + 1;
+            for (int j = 0; j < i; j++) {
+                if (lottoList[i] == temp) {
+                    temp = rd.nextInt(45) + 1;
+                    j = j - 1;
+                }
+            }
+            lottoList[i] = temp;
+        }
+        System.out.print(Arrays.toString(lottoList));
+    }
+}
